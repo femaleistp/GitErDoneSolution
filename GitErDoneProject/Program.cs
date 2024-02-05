2e1ab09bc85aad9fd65af7f6f1e5e10d59e36755
Brittany Hancock <brittany.hancock@student.centralia.edu>
2024-02-05 11:46:19 -0800

    Added new write and attached a readme file

--- GitErDoneProject/Program.cs
+++ GitErDoneProject/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); //comments
+            Console.WriteLine("Hello, World!"); //comments comments
         }
     }
 }
