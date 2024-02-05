fc9e48d174b0a22fe55756cf59aa534801616506
Brittany Hancock <brittany.hancock@student.centralia.edu>
2024-02-05 11:43:17 -0800

    changed again

--- GitErDoneProject/Program.cs
+++ GitErDoneProject/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); //comment
+            Console.WriteLine("Hello, World!"); //comments
         }
     }
 }
